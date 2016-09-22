38e544e68bc4a126a11ebfab677448cb782ff9c4
Kurt Weber <weberkrt@msn.com>
2016-09-21 20:38:28 -0400

    Added using(reader) { }

--- StreamReaderTask2/Program.cs
+++ StreamReaderTask2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
+using System.IO; // Remember to add this line when using StreamReader/StreamWriter
 
 namespace StreamReaderTask2
 {
@@ -13,25 +13,27 @@ namespace StreamReaderTask2
         {
             // Create an instance of StreamReader to read from a file
             StreamReader reader = new StreamReader("..\\..\\..\\Shakespeare.txt");
-
-            // Count to determine number of lines
-            int lineNumber = 0;
-            
-            // Read first line from the text file
-            string line = reader.ReadLine();
-
-            // Read the other lines from the text file
-            while (line != null)
+            using (reader)  // Auto-Closing between {}  do not need reader.Close();
             {
-                // Increment by 1 (linenumber = linenumber + 1)
-                lineNumber++;
-                // Write line to console
-                Console.WriteLine(line);
-                // Read next line
-                line = reader.ReadLine();
+                // Count to determine number of lines
+                int lineNumber = 0;
+
+                // Read first line from the text file
+                string line = reader.ReadLine();
+
+                // Read the other lines from the text file
+                while (line != null)
+                {
+                    // Increment by 1 (linenumber = linenumber + 1)
+                    lineNumber++;
+                    // Write line to console
+                    Console.WriteLine(line);
+                    // Read next line
+                    line = reader.ReadLine();
+                }
+                // Close the resource after you've finished using it
+                //reader.Close();
             }
-            // Close the resource after you've finished using it
-            reader.Close();
         }
     }
 }
